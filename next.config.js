d48a64f2284c6fd19ff83f44f1228e73df4efc22
vuhongduc911 <vuhongduc12a4@gmail.com>
2023-02-17 22:16:00 +0700

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://root:sapassword@cluster0.gcqtzfo.mongodb.net/?retryWrites=true&w=majority",
+        "MONGODB_URL": "mongodb+srv://root:sapassword@cluster0.zo3mqh7.mongodb.net/?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": ".q~C<9G4jYzTZCBT!y,[V?3x,:.2'CD!J`S+?7kAm4SY%3{R>a",
         "REFRESH_TOKEN_SECRET": "QEQr]crS.h,[MERm2Z'`}!(YrTQ^>Yt!xCcL#xc2WFgk?W4~b,UzWtAHe8[s<a5xjcn5US+M'7_b",
-        "PAYPAL_CLIENT_ID": "ASYkfapNje3z9kEZAjtklIcsrkJYCmrGGUrvGTzDNFC67CTapQaBRDkETel2iNEXdlEWSQQ12LpP6Otc",
-        "CLOUD_UPDATE_PRESET": "hongduc981",
-        "CLOUD_NAME": "dkpag5qlm",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/dkpag5qlm/image/upload"
+        "PAYPAL_CLIENT_ID": "AYYWU3fLangt4AmM3R623zSdKJXhqbdGv13k3WzNPETdMvmoqSnruVZrfMxGUkR595mijTJFa87dr4ju",
+        "CLOUD_UPDATE_PRESET": "vanhieu981",
+        "CLOUD_NAME": "dzygop1hq",
+        "CLOUD_API": "https://api.cloudinary.com/v1_1/dzygop1hq/image/upload"
     }
 
 
